a46fade38073cbcbccc55171dd3d1b1cc3cf2b99
williamgoupil <will.goupil@gmail.com>
2021-02-08 09:16:12 -0500

    Creation des methodes de BWPixel

--- TP1/src/BWPixel.java
+++ TP1/src/BWPixel.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * @author Pier-Alexandre Carron, William Goupil
  * @version 1.0
@@ -13,22 +17,34 @@ public class BWPixel extends Pixel
      */
     public BWPixel(int c)
     {
-        ReadPixel(c);
+        readPixel(c);
     }
     /**
      * Attribue la valeur à color1
      * @param c code de couleur, doit être un int
      */
-    public void ReadPixel(int c)
+    public void readPixel(int c)
     {
         this.color1 = c;
     }
-
+    public void writePixel(File f)
+    {
+        FileWriter writer = null;
+        try
+        {
+            writer = new FileWriter(f);
+            writer.write(this.getCodeValue());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
     /**
      *  envoie valuer color1
      * @return valeur de c1, est un int
      */
-    public int getCodeValue()
+    private int getCodeValue()
     {
         return this.color1;
     }
@@ -37,9 +53,9 @@ public class BWPixel extends Pixel
      * Modifie la valeur courante de c
      * @param v difference de modification, doit être un int
      */
-    public static void darken_Brigthen(int v)
+    public void darken_Brigthen(int v)
     {
-
+        readPixel(getCodeValue() - v);
     }
 
     /**
@@ -47,8 +63,11 @@ public class BWPixel extends Pixel
      * @param p pixel a comparer
      * @return boolean de comparaison
      */
-    public static boolean pixelsTheSame(Pixel p)
+    public boolean pixelsTheSame(Pixel p)
     {
-        return true;
+        if(p instanceof BWPixel)
+            return ((BWPixel) p).getCodeValue() == this.getCodeValue();
+
+        return false;
     }
 }
